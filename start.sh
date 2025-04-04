dc4c949ac2eb51e00839e1943217ef2d37689f80
Fatimah Emad El-Den <63984422+astral-fate@users.noreply.github.com>
2025-04-04 07:58:24 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 # start.sh - Wrapper script to ensure correct port binding
 
 echo "Starting application with PORT=$PORT"
-gunicorn --log-level debug --workers 1 --threads 4 --timeout 120 --access-logfile - --error-logfile - --bind 0.0.0.0:8080 wsgi:application
+gunicorn --log-level debug --workers 1 --threads 4 --timeout 120 --access-logfile - --error-logfile - --bind 0.0.0.0:8000 wsgi:application
